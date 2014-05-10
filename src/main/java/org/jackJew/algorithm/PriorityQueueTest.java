963e2dca48f415fed2606b63582813171d6812d2
runjia <runjiamail@gmail.com>
2014-05-10 11:35:44 +0800

    describe more details about PriorityQueue implementation

--- src/main/java/org/jackJew/algorithm/PriorityQueueTest.java
+++ src/main/java/org/jackJew/algorithm/PriorityQueueTest.java
@@ -2,6 +2,9 @@ package org.jackJew.algorithm;
 
 import java.util.PriorityQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.DelayQueue;
+import java.util.TreeMap;
 import java.util.concurrent.SynchronousQueue;
 
 public class PriorityQueueTest {
@@ -10,6 +13,8 @@ public class PriorityQueueTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		// default init_capacity is 11.
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 		pq.add(999);
 		pq.add(100);
@@ -18,10 +23,12 @@ public class PriorityQueueTest {
 		pq.add(4);
 		pq.add(-5);
 		// add: siftUp,  k = size,  (k-1) >> 1; while loop, find greater than
+		// add: always maintain the 0-index element as the least by siftUp method.
 		
 		Integer ele = 0;
 		while( (ele = pq.poll()) != null ) {
 			// poll: siftDown,  k = 0 ; (k+1) << 1,while loop, find less than
+			// poll: always poll the least element by siftDown method before return
 			System.out.println(ele);
 		}
 		
