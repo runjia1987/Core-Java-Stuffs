e1528c9e1417029a24d357c21164ceef4049a045
runjia <runjiamail@gmail.com>
2014-05-27 16:21:35 +0800

    MVEL unit test

---
+++ src.test.java/org/jack/MVEL/test/MVEL_Test.java
@@ -0,0 +1,18 @@
+package org.jack.MVEL.test;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MVEL_Test {
+	
+	@BeforeClass
+	public static void beforeClass(){
+		//
+	}
+	
+	@Test
+	public void propertyExpression (){
+		
+	}
+
+}
