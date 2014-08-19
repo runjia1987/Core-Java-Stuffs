004ab4512ca9fd4548a654014b18d4e708b772fd
runjia <runjiamail@gmail.com>
2014-08-19 16:46:42 +0800

    programmatic transaction management

--- src/main/java/org/jackJew/AOP/transaction/programmatic/TransactionService.java
+++ src/main/java/org/jackJew/AOP/transaction/programmatic/TransactionService.java
@@ -1,6 +1,7 @@
 package org.jackJew.AOP.transaction.programmatic;
 
 import org.apache.log4j.Logger;
+import org.springframework.dao.DataAccessException;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -17,25 +18,23 @@ public class TransactionService {
 	
 	public void service1(){
 		DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
-		TransactionStatus status = null;
-		try {
-			System.out.println("calling PojoDao.");
-			definition.setIsolationLevel(TransactionDefinition.ISOLATION_SERIALIZABLE);
-			definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_NESTED);
-			definition.setReadOnly(true);
-			definition.setTimeout(5);  // 5s timeout
-			
-			status = transactionManager.getTransaction(definition);
+		
+		System.out.println("calling PojoDao.");
+		definition.setIsolationLevel(TransactionDefinition.ISOLATION_SERIALIZABLE);
+		definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_NESTED);
+		definition.setReadOnly(true);
+		definition.setTimeout(5);  // 5s timeout
+		TransactionStatus status = transactionManager.getTransaction(definition);
+		
+		try {			
 			// jdbcTemplate or Ibatis SqlMapClient
-			transactionManager.commit(status);
 			
-		} catch(Exception e){
-			System.out.println(e.getLocalizedMessage());
-			if(status != null){
-				transactionManager.rollback(status);
-			}
-			System.out.println("service1 rollback complete.");
+		} catch(DataAccessException e){
+			System.out.println(e.getMessage());
+			transactionManager.rollback(status);			
+			throw e;
 		}
+		transactionManager.commit(status);
 	}
 
 	public PlatformTransactionManager getTransactionManager() {
