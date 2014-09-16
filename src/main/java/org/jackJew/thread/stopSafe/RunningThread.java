f48b1518feca733d2c0f8213705bb3d456050baf
runjia <runjiamail@gmail.com>
2014-09-16 21:52:16 +0800

    thread interrupt test

--- src/main/java/org/jackJew/thread/stopSafe/RunningThread.java
+++ src/main/java/org/jackJew/thread/stopSafe/RunningThread.java
@@ -20,14 +20,7 @@ public class RunningThread extends Thread {
 				result = 90.0d;
 			else
 				result *= result;
-			System.out.println(result);
-			
-			//int x = 1 / 0;	//unchecked exception会导致本线程终止, terminated
-			try {
-				Thread.sleep(1200);
-			} catch (InterruptedException e) {	//捕获异常 (checked Exception)
-				System.out.println("catch an InterruptedException!");
-			}
+			System.out.println(result);			
 		}
 	}
 
--- src/main/java/org/jackJew/thread/stopSafe/TestClient.java
+++ src/main/java/org/jackJew/thread/stopSafe/TestClient.java
@@ -12,8 +12,8 @@ public class TestClient {
 		rt.start();
 		
 		Thread.sleep(4000);
-		rt.setIsRun(false);	//设置共享变量标识
-		rt.interrupt();		//调用中断方法
+		//rt.setIsRun(false);	//设置共享变量标识 , required
+		rt.interrupt();		//调用中断方法, will not end the thread unless call setIsRun(false)
 	}
 
 }
