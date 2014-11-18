437db026059c95766ae84b7f6d5ea0e4c3e8d0f2
runjia <runjiamail@gmail.com>
2014-11-18 17:53:13 +0800

    comment sth.

--- src/main/java/org/jackJew/algorithm/SortAlgorithm.java
+++ src/main/java/org/jackJew/algorithm/SortAlgorithm.java
@@ -7,177 +7,185 @@ import java.util.Stack;
 
 /**
  * 
- * Description: 排序算法实现 
+ * Description: 排序算法实现
+ * 
  * @author zhurunjia
  *
  */
 public class SortAlgorithm {
-	
-	private static int[] array = {12,45,67,90,103,666,2,103,88,344,45,1000};
+
+	private static int[] array = { 12, 45, 67, 90, 103, 666, 2, 103, 88, 344, 45, 1000 };
 	final static Random rand = new Random();
-	
+
 	/**
 	 * 归并排序, 递归
 	 */
-	public static void mergeSort(int left, int right, int[] array, int[] temp){
-		if(left < right){
-			int middle = (left + right)/2;  //中间点
-			
-			mergeSort(left, middle, array, temp);	//左侧排序
-			mergeSort(middle+1, right, array, temp); //右侧排序
-			
-			mergeArray(left, right, array, temp);	//合并已排序的左侧与右侧数据
+	public static void mergeSort(int left, int right, int[] array, int[] temp) {
+		if (left < right) {
+			int middle = (left + right) / 2; // 中间点
+
+			mergeSort(left, middle, array, temp); // 左侧排序
+			mergeSort(middle + 1, right, array, temp); // 右侧排序
+
+			mergeArray(left, right, array, temp); // 合并已排序的左侧与右侧数据
 		}
 	}
+
 	/**
 	 * 递增有序
 	 */
-	public static void mergeArray(int left, int right, int[] array, int[] temp){
-		int i = left, mid = (left+right)/2, j = mid+1, index = 0;
-		while(i <= mid && j <= right) {
-			if(array[i] < array[j])
+	public static void mergeArray(int left, int right, int[] array, int[] temp) {
+		int i = left, mid = (left + right) / 2, j = mid + 1, index = 0;
+		while (i <= mid && j <= right) {
+			if (array[i] < array[j])
 				temp[index++] = array[i++];
 			else
 				temp[index++] = array[j++];
 		}
-		while(i <= mid)
+		while (i <= mid)
 			temp[index++] = array[i++];
-		while(j <= right)
+		while (j <= right)
 			temp[index++] = array[j++];
-		
-		for(i=0; i<index; i++)
-			array[left+i] = temp[i];
+
+		for (i = 0; i < index; i++)
+			array[left + i] = temp[i];
 	}
-	
+
 	/**
 	 * merge and find out the identical elements from two sorted arrays
 	 */
-	public static void mergeCommon(int[] array1, int[] array2){
-		int a1 = 0, a2 = array1.length, b1 = 0 ,b2 = array2.length;
+	public static void mergeCommon(int[] array1, int[] array2) {
+		int a1 = 0, a2 = array1.length, b1 = 0, b2 = array2.length;
 		int c1, c2;
-		while(a1 < a2 && b1 < b2) {
+		while (a1 < a2 && b1 < b2) {
 			c1 = array1[a1];
 			c2 = array2[b1];
 			if (c1 == c2) {
 				System.out.println(c1);
 				a1++;
 				b1++;
-			} else if( c1 < c2) {
+			} else if (c1 < c2) {
 				a1++;
 			} else {
 				b1++;
 			}
 		}
 	}
-	
-	
+
 	/**
 	 * 快速排序, ASC
 	 */
-	public static void quickSort(int[] array, int left, int right){
-		int i = left, j = right, pivot = array[left];	//选择一个参照数
-		
-		if(left < right){			
-			while (i < j){
-				while(i < j && array[j] >= pivot)	//从右端点向左查找第一个小于参照数的值
+	public static void quickSort(int[] array, int left, int right) {
+		int i = left, j = right, pivot = array[left]; // 选择一个参照数
+
+		if (left < right) {
+			while (i < j) {
+				while (i < j && array[j] >= pivot)
+					// 从右端点向左查找第一个小于参照数的值
 					j--;
-				if(i < j)
+				if (i < j)
 					array[i] = array[j];
-				while(i < j && array[i] < pivot)	//从左端点向右查找第一个大于=参照数的值
+				while (i < j && array[i] < pivot)
+					// 从左端点向右查找第一个大于=参照数的值
 					i++;
-				if(i < j)
+				if (i < j)
 					array[j] = array[i];
 			}
-			
-			array[i] = pivot;	//第一轮排序后, i左侧的数都小于i右侧的数据
-			
-			quickSort(array, left, i-1);	//排序左侧数据
-			quickSort(array, i+1, right);	//排序右侧数据
+
+			array[i] = pivot; // 第一轮排序后, i左侧的数都小于i右侧的数据
+
+			quickSort(array, left, i - 1); // 排序左侧数据
+			quickSort(array, i + 1, right); // 排序右侧数据
 		}
 	}
-	
+
 	/**
 	 * 快速排序, ASC, 随机化轴值
 	 */
-	public static void quickSort_random(int[] array, int left, int right){
-		if(left < right){
-			int randomPos = left + rand.nextInt(right-left);
-			int i = left, j = right, X = array[i], pivot = array[randomPos];	//随机的参照数
+	public static void quickSort_random(int[] array, int left, int right) {
+		if (left < right) {
+			int randomPos = left + rand.nextInt(right - left);
+			int i = left, j = right, X = array[i], pivot = array[randomPos]; // 随机的参照数
 			// swap
 			array[randomPos] = X;
 			array[i] = pivot;
-			
-			while (i < j){
-				while(i < j && array[j] >= pivot)	//从右端点向左查找第一个小于参照数的值
+
+			while (i < j) {
+				while (i < j && array[j] >= pivot)
+					// 从右端点向左查找第一个小于参照数的值
 					j--;
-				if(i < j)
+				if (i < j)
 					array[i] = array[j];
-				while(i < j && array[i] < pivot)	//从左端点向右查找第一个大于参照数的值
+				while (i < j && array[i] < pivot)
+					// 从左端点向右查找第一个大于参照数的值
 					i++;
-				if(i < j)
+				if (i < j)
 					array[j] = array[i];
 			}
-			
-			array[i] = pivot;	//第一轮排序后, i左侧的数都小于i右侧的数据
-			
-			quickSort_random(array, left, i-1);	//排序左侧数据
-			quickSort_random(array, i+1, right);	//排序右侧数据
+
+			array[i] = pivot; // 第一轮排序后, i左侧的数都小于i右侧的数据
+
+			quickSort_random(array, left, i - 1); // 排序左侧数据
+			quickSort_random(array, i + 1, right); // 排序右侧数据
 		}
 	}
-	
+
 	/**
-	 * 快速排序, 非递归
+	 * 快速排序, 非递归. <br>
+	 * 快排可用于查找中位数 median value.
 	 */
 	public static void quickSort_nonRecursive(int[] array, int left, int right) {
 		Stack<Integer> stack = new Stack<Integer>();
 		int index = findPivotIndex(array, left, right);
-		
+
 		if (index != -1 && left < right) {
 			if (left + 1 < index) {
 				stack.push(left);
 				stack.push(index - 1);
-			} 
-			if (index + 1 < right){
+			}
+			if (index + 1 < right) {
 				stack.push(index + 1);
 				stack.push(right);
 			}
-			
+
 			while (stack.size() > 0) {
 				int highBound = (Integer) stack.pop();
 				int lowBound = (Integer) stack.pop();
 				index = findPivotIndex(array, lowBound, highBound);
 				if (index == -1)
 					break;
-				//repeat
+				// repeat
 				if (lowBound + 1 < index) {
 					stack.push(lowBound);
 					stack.push(index - 1);
-				} 
-				if (index + 1 < highBound){
+				}
+				if (index + 1 < highBound) {
 					stack.push(index + 1);
 					stack.push(highBound);
 				}
 			}
 		}
 	}
-	
+
 	// find the index for pivot
 	private static int findPivotIndex(int[] array, int left, int right) {
 		if (left < right) {
 			int i = left, j = right;
+			// random pivot pos
 			int randomPos = left + new Random().nextInt(right - left);
 			int pivot = array[i];
+			// switch with left-most pos
 			array[i] = array[randomPos];
 			array[randomPos] = pivot;
 			pivot = array[i];
-			
-			while ( i < j) {
-				while(i < j && array[j] >= pivot)
+
+			while (i < j) {
+				while (i < j && array[j] >= pivot)
 					j--;
 				if (i < j)
 					array[i] = array[j];
-				
-				while ( i< j && array[i] < pivot)
+
+				while (i < j && array[i] < pivot)
 					i++;
 				array[j] = array[i];
 			}
@@ -187,32 +195,33 @@ public class SortAlgorithm {
 			return -1;
 		}
 	}
-	
+
 	/**
 	 * 冒泡排序
 	 */
-	public static void bubbleSort(int[] array, int length){
+	public static void bubbleSort(int[] array, int length) {
 		int temp;
-		for(int i = 1; i < length -1; i++){
-			for(int j = 0; j < length - i; j++){
-				if(array[j] > array[j+1]){	//相邻元素不断比较交换
+		for (int i = 1; i < length - 1; i++) {
+			for (int j = 0; j < length - i; j++) {
+				if (array[j] > array[j + 1]) { // 相邻元素不断比较交换
 					temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * 选择排序
 	 */
-	public static void selectSort(int[] array, int length){
+	public static void selectSort(int[] array, int length) {
 		int temp, minJ;
-		for(int i = 0; i < length-1; i++){
-			temp = array[i]; minJ = i;
-			for(int j = i+1; j < length; j++){  //将本次循环的最小值与当前首元素交换
-				if(array[i] > array[j]){
+		for (int i = 0; i < length - 1; i++) {
+			temp = array[i];
+			minJ = i;
+			for (int j = i + 1; j < length; j++) { // 将本次循环的最小值与当前首元素交换
+				if (array[i] > array[j]) {
 					minJ = j;
 				}
 			}
@@ -220,29 +229,29 @@ public class SortAlgorithm {
 			array[minJ] = temp;
 		}
 	}
-	
+
 	/**
 	 * 排序测试
 	 */
 	public static void main(String[] args) throws Exception {
 		int[] temp = new int[array.length];
-		
-		//mergeSort(0, array.length-1, array, temp);
-		
-		//quickSort(array, 0, array.length-1);
-		quickSort_nonRecursive(array, 0, array.length-1);
+
+		// mergeSort(0, array.length-1, array, temp);
+
+		// quickSort(array, 0, array.length-1);
+		quickSort_nonRecursive(array, 0, array.length - 1);
 		System.out.println(Arrays.toString(array));
-				
-		//bubbleSort(array, array.length);
+
+		// bubbleSort(array, array.length);
 		selectSort(array, array.length);
 		System.out.println(Arrays.toString(array));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
 		System.out.println(sdf.parse("2013-12-16 20:09:33.0"));
-		
-		int[] array1 = {-9, 1, 7, 45, 234, 321}, array2 = {7, 56, 321, 500, 600, 1000, 4500, };
+
+		int[] array1 = { -9, 1, 7, 45, 234, 321 }, array2 = { 7, 56, 321, 500, 600, 1000, 4500, };
 		mergeCommon(array1, array2);
-		
+
 	}
 
 }
