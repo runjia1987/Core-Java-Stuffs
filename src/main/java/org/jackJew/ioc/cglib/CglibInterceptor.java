099bbcab10406413a31a7b5f3aa01fc3df52c0ef
runjia1987 <runjiamail@gmail.com>
2015-12-30 14:30:40 +0800

    cglib MethodInterceptor

--- src/main/java/org/jackJew/ioc/cglib/CglibInterceptor.java
+++ src/main/java/org/jackJew/ioc/cglib/CglibInterceptor.java
@@ -12,21 +12,21 @@ import net.sf.cglib.proxy.MethodProxy;
  * cglib动态派生自目标对象类的子类 , instance of dynamic subclassing
  * @author zhurunjia
  */
-public class CglibProxyFactory implements MethodInterceptor {
+public class CglibInterceptor implements MethodInterceptor {
 	private Object targetObject;
 
 	@Override
 	public Object intercept(Object proxy, Method method, Object[] params,
 			MethodProxy methodProxy) throws Throwable {
 		
-		System.out.println("intercept()拦截的方法：" + method.getName());
-		System.out.println("代理对象实现的父类：" + proxy.getClass().getSuperclass().getName());
+		System.out.println("拦截的方法名：" + method.getName());
+		System.out.println("参数值：" + Arrays.toString(params));
 		System.out.println("实现的接口: " + Arrays.toString(proxy.getClass().getInterfaces()));
 		
 		if(method.getName().startsWith("step")) {	//过滤方法
 			return null;
 		}
-		
+		// invokeSuper是退出当前interceptor的处理，进入下一个callback处理; invoke则会继续回调该方法
 		return methodProxy.invoke(this.targetObject, params);
 	}
 	
@@ -41,8 +41,6 @@ public class CglibProxyFactory implements MethodInterceptor {
 		// if default no-args isn't defined, use this instead
 		Constructor<?> ctr = obj.getClass().getConstructors()[0];
 		return enhancer.create(ctr.getParameterTypes(), new Object[]{ 1 });
-		
-		//return enhancer.create();
 	}
 
 }
--- src/main/java/org/jackJew/ioc/cglib/TestClient.java
+++ src/main/java/org/jackJew/ioc/cglib/TestClient.java
@@ -6,9 +6,9 @@ public class TestClient {
 	 * 
 	 */
 	public static void main(String[] args) {
-		CglibProxyFactory factory = new CglibProxyFactory();
+		CglibInterceptor interceptor = new CglibInterceptor();
 		
-		ServiceImpl s =(ServiceImpl) factory.createProxy(new ServiceImpl(1));
+		ServiceImpl s =(ServiceImpl) interceptor.createProxy(new ServiceImpl(1));
 		s.service("测试消息输出");
 		s.step1();
 	}
