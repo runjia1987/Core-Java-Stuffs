a70e6380a8e1eb12ced0d754db8a0898a7a1e70f
zhurunjia192 <zhurunjia192@lu.com>
2016-07-27 14:46:52 +0800

    方法异常签名的多接口实现

--- src/main/java/org/jackJew/interview/MethodExceptionSignature.java
+++ src/main/java/org/jackJew/interview/MethodExceptionSignature.java
@@ -13,6 +13,10 @@ public class MethodExceptionSignature {
 		@Override
 		public void print2() { //取两个接口相同方法的声明异常的 /*交集*/ 或不声明
 		}
+
+		@Override
+		public void print3() throws RuntimeException { //取两个接口相同方法的声明异常的 /*交集*/ 或不声明
+		}
 		
 	}
 
@@ -21,8 +25,10 @@ public class MethodExceptionSignature {
 interface Parent {
 	abstract void print() throws InterruptedException;
 	abstract void print2() throws IOException;
+	abstract void print3() throws Exception;
 }
 
 interface Parent2 {
 	abstract void print2() throws RuntimeException;
+	abstract void print3() throws RuntimeException;
 }
